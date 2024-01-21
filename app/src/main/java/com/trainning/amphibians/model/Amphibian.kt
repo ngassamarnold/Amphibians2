b1b9bd0c72a1c9ca646944a2f320f770f48c0b85
Arnold Ngassam <ngassam.arnold@nanobnk.com>
2024-01-21 08:04:08 +0400

    Add class Amphibian

--- app/src/main/java/com/trainning/amphibians/model/Amphibian.kt
+++ app/src/main/java/com/trainning/amphibians/model/Amphibian.kt
@@ -1,8 +1,11 @@
-package com.trainning.amphibians.network
+package com.trainning.amphibians.model
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * This data class defines a Amphibians .
+ */
 @Serializable
 data class Amphibian(
     val name: String,
